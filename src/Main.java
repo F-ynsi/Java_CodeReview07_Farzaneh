acc86a4553ae03921becbc5a62f01c5f23cf3853
Farzaneh Younesi <farzanehynsi@gmail.com>
2020-08-15 16:42:48 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.sql.*;
+import java.util.*;
+
 public class Main {
 
+    static DataAccess dataAccess;
+
     public static void main(String[] args) {
-	// write your code here
+
+        init();
+        new Menu(dataAccess);
+        stop();
     }
+
+
+    static public void init(){
+        try{
+            dataAccess = new DataAccess();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    static public void stop(){
+        try{
+            dataAccess.closeDB();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
